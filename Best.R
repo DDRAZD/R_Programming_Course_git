5a9e3e4fec929f239a890b99ef0db5597acf3a82
Yaniv Nathan <yanivnathan@gmail.com>
2017-04-16 02:43:28 -0400

    added dealing with empty values on read.csv which fixed all the sorts

--- Best.R
+++ Best.R
@@ -2,7 +2,7 @@
 
 best <- function(state, outcome) {
         ## Read outcome data
-        outome<-read.csv("outcome-of-care-measures.csv")
+        outcome_df<-read.csv("outcome-of-care-measures.csv", na.strings="Not Available", stringsAsFactors=FALSE)
         
         ## Check that state and outcome are valid
         ## The hospital name is the name provided in the Hospital.Name variable. 
@@ -20,6 +20,20 @@ best <- function(state, outcome) {
         ## Return hospital name in that state with lowest 30-day death
         ## rate
         
-        ##handling ties
+        ## take only relevant state
+        relevant_state<-subset.data.frame(outcome_df,outcome_df$State==state)
+       
+       
+        if(outcome =="heart attack") { aranged_ouctome<-arrange(relevant_state, 
+                                                                Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,
+                                                                Hospital.Name)}
+        if(outcome =="heart failure") { aranged_ouctome<-arrange(relevant_state,
+                                                                Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,
+                                                                Hospital.Name)} 
+        if(outcome =="pneumonia")    { 
+               
+                aranged_ouctome<-arrange(relevant_state,Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia,Hospital.Name)}
+        
+        as.character(aranged_ouctome[1,2])
         
 }
