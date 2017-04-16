b7aa219d4d94b6e75205be71628bc5187ef94001
Yaniv Nathan <yanivnathan@gmail.com>
2017-04-16 15:13:03 -0400

    starting phase 3 of the last assignments with ranking hospitals

---
+++ rankhospital.R
@@ -0,0 +1,17 @@
+
+
+rankhospital <- function(state, outcome, num = "best") {
+        ## Read outcome data
+        
+        
+        
+        ## Check that state and outcome are valid
+        
+        ##The function should check the validity of its arguments. If an invalid 
+        ##state value is passed to rankhospital, the function should throw an 
+        ##error via the stop function with the exact message "invalid state".
+        ##If an invalid outcome value is passed to rankhospital, the function 
+        ##should throw an error via the stop function with the exact message
+        ##"invalid outcome" Return hospital name in that state with the given rank
+        ## 30-day death rate
+}
